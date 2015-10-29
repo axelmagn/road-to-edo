07025c992225d7d487c4e18c2581096eb5f8efb2
Axel Magnuson <axelmagn@gmail.com>
2015-10-29 17:06:03 -0600

    dunno

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 extern crate piston;
+extern crate piston_window;
 extern crate toml;
 
+use std::io::{Read};
 use std::fs::File;
 use std::path::Path;
 
-use piston::WindowSettings;
+use piston_window::{WindowSettings, PistonWindow, clear};
 
-const DEFAULT_CONF_PATH: Path = Path::new("conf");
-const DEFAULT_SETTINGS_CONF: Path = Path::new("engine.toml");
+const DEFAULT_CONF_PATH: &'static str = "conf";
+const DEFAULT_SETTINGS_CONF: &'static str = "settings.toml";
 
 fn main() {
     // load environment config
     let settings = {
-        let settings = DEFAULT_CONF_PATH.join(DEFAULT_SETTINGS_CONF);
-        let settings = File::open(settings);
+        let settings = Path::new(DEFAULT_CONF_PATH).join(DEFAULT_SETTINGS_CONF);
+        let mut settings = File::open(settings).unwrap();
         let mut buf = String::new();
         settings.read_to_string(&mut buf).unwrap();
         toml::Parser::new(&buf).parse().unwrap()
     };
-    println!("{:?}", settings);
-    
+
     // Create a new window 
+    let width: u32 = settings["window"].lookup("width").unwrap()
+        .as_integer().unwrap() as u32;
+    let height: u32 = settings["window"].lookup("height").unwrap()
+        .as_integer().unwrap() as u32;
+    let window: PistonWindow = WindowSettings::new("Hello Piston!", 
+                                                   (width, height))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+    for e in window {
+        e.draw_2d(|_c, g| {
+            clear([0.5, 1.0, 0.5, 1.0], g);
+        });
+    }
 }
